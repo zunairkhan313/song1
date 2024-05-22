8fad0bbf2d9e25390a649de69eb53d8f678ed9de
Zunair Khan <121635619+zunairkhan313@users.noreply.github.com>
2024-05-22 03:53:49 -0700

    committed

--- app/Components/Banner.jsx
+++ app/Components/Banner.jsx
@@ -3,8 +3,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Banner1 from "../../public/Images/Banner.jpg"
 import Banner2 from "../../public/Images/Banner1.jpg"
-// import Banner2 from "../../public/Images/Banner2.jpg"
-// import Banner3 from "../../public/Images/Banner3.jpg"
 import Image from 'next/image';
 
 export default function CarouselPage() {
